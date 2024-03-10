6e14f9ddaf14b83eaeb34ccaec3133fa41b87cf0
Dragonkuro2 <hicham.bourezi@gmail.com>
2024-03-10 07:18:42 -0700

    I added sudo permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 mkdir -p data/web_static/shared data/web_static/releases/test
 echo 'Hello World!' | sudo tee /data/web_static/releases/test/index.html
-sudo n -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
