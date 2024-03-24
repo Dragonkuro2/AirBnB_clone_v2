074732a5238362cf27f352ac98d1f6450fa249e3
Dragonkuro2 <hicham.bourezi@gmail.com>
2024-03-24 09:37:18 -0700

    I fixed the errors of pycodestyle checker and I made the code respect the 'PEP 8' style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
 script that starts a Flask web application
-	on IP 0.0.0.0 and port 5000
+on IP 0.0.0.0 and port 5000
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-	""" a simple function that returns Hello HBNB! """
-	return "Hello HBNB!"
+    """ a simple function that returns Hello HBNB! """
+    return "Hello HBNB!"
 
-if __name__ ==  '__main__':
-	app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
