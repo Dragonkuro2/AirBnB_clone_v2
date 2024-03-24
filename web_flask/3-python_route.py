025d8c98fc4869162fe0edf3761e67663d36154b
Dragonkuro2 <hicham.bourezi@gmail.com>
2024-03-24 11:25:38 -0700

    task number 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def C_route(text):
     """ Function that returns 'C' + the rest of the link """
     return "C %s" % text.replace("_", " ")
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_is_cool(text='is cool'):
     """ Function that resturs 'python' + the rest of the link
