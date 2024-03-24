43d5926c5384a33ade363744da9c4ebd8ed0d6fd
Dragonkuro2 <hicham.bourezi@gmail.com>
2024-03-24 10:46:04 -0700

    task number 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" script that listen on IP 0.0.0.0 and port 5000
+    using flask, and it'll display something on required page
+    depands on the URL, """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Simple function that returns 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Function that returns 'HBNB'. """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_route():
+    """ Function that returns 'C' + the rest of the link """
+    return "C %s" % text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
